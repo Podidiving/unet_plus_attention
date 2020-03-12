00e81d045fdb4538c123ba613b1b153b3c32e84a
Aleks <veselev.aleks@gmail.com>
2020-03-12 08:41:04 +0300

    add self-attention block with skip connection and depthwise separable conv block

--- unet_plus_attention/model/model.py
+++ unet_plus_attention/model/model.py
@@ -30,3 +30,57 @@ def get_unet_attention_decoder(in_dim: int, out_dim: int) -> nn.Module:
         'dec4': attention_block_build,
     }
     return Unet(module_builders, in_dim, out_dim)
+
+
+def get_unet_attention_with_skip_connections_decoder(
+        in_dim: int,
+        out_dim: int
+) -> nn.Module:
+    module_builders = {
+        'enc1': vanila_block_build,
+        'enc2': vanila_block_build,
+        'enc3': vanila_block_build,
+        'enc4': vanila_block_build,
+        'enc5': vanila_block_build,
+        'dec1': attention_with_skip_connection_block_build,
+        'dec2': attention_with_skip_connection_block_build,
+        'dec3': attention_with_skip_connection_block_build,
+        'dec4': attention_with_skip_connection_block_build,
+    }
+    return Unet(module_builders, in_dim, out_dim)
+
+
+def get_unet_depthwise_encoder_attention_with_skip_connections_decoder(
+        in_dim: int,
+        out_dim: int
+) -> nn.Module:
+    module_builders = {
+        'enc1': depthwise_separable_block_build,
+        'enc2': depthwise_separable_block_build,
+        'enc3': depthwise_separable_block_build,
+        'enc4': depthwise_separable_block_build,
+        'enc5': depthwise_separable_block_build,
+        'dec1': attention_with_skip_connection_block_build,
+        'dec2': attention_with_skip_connection_block_build,
+        'dec3': attention_with_skip_connection_block_build,
+        'dec4': attention_with_skip_connection_block_build,
+    }
+    return Unet(module_builders, in_dim, out_dim)
+
+
+def get_unet_depthwise_light_encoder_attention_with_skip_connections_decoder(
+        in_dim: int,
+        out_dim: int
+) -> nn.Module:
+    module_builders = {
+        'enc1': depthwise_separable_block_light_build,
+        'enc2': depthwise_separable_block_light_build,
+        'enc3': depthwise_separable_block_light_build,
+        'enc4': depthwise_separable_block_light_build,
+        'enc5': depthwise_separable_block_light_build,
+        'dec1': attention_with_skip_connection_block_build,
+        'dec2': attention_with_skip_connection_block_build,
+        'dec3': attention_with_skip_connection_block_build,
+        'dec4': attention_with_skip_connection_block_build,
+    }
+    return Unet(module_builders, in_dim, out_dim)
--- unet_plus_attention/model/unet_blocks_builder.py
+++ unet_plus_attention/model/unet_blocks_builder.py
@@ -26,3 +26,74 @@ def attention_block_build(in_: int, out: int) -> nn.ModuleList:
         nn.BatchNorm2d(out),
         nn.ReLU()
     ])
+
+
+class AttentionWithSkipConnection(nn.Module):
+    def __init__(self, in_: int, out: int):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_, in_ // 4, kernel_size=1),
+            nn.BatchNorm2d(in_ // 4),
+            nn.ReLU(),
+            SelfAttentionBlock(in_ // 4, out // 4, kernel_size=5, stride=1, padding=2, groups=4),
+            nn.BatchNorm2d(out // 4),
+            nn.ReLU(),
+            nn.Conv2d(out // 4, out, kernel_size=1),
+            nn.BatchNorm2d(out),
+            nn.ReLU()
+        )
+        if in_ != out:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_, out, kernel_size=1, stride=1, padding=0),
+                nn.BatchNorm2d(out)
+            )
+        else:
+            self.downsample = None
+
+    def forward(self, x):
+        out = self.seq(x)
+        if self.downsample is not None:
+            out += self.downsample(x)
+        else:
+            out += x
+        return out
+
+
+class DepthWiseSeparableConv(nn.Module):
+    def __init__(self, in_, out, kernel_size=3, stride=1, padding=0):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_, in_, kernel_size=kernel_size,
+                stride=stride, padding=padding, groups=in_
+            ),
+            nn.ReLU(),
+            nn.BatchNorm2d(in_),
+            nn.Conv2d(in_, out, kernel_size=1)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+def attention_with_skip_connection_block_build(in_: int, out: int) -> nn.ModuleList:
+    return nn.ModuleList([
+        AttentionWithSkipConnection(in_, out)
+    ])
+
+
+def depthwise_separable_block_build(in_: int, out: int) -> nn.ModuleList:
+    return nn.ModuleList([
+        DepthWiseSeparableConv(in_, out, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(out),
+        DepthWiseSeparableConv(out, out, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(out),
+    ])
+
+
+def depthwise_separable_block_light_build(in_: int, out: int) -> nn.ModuleList:
+    return nn.ModuleList([
+        DepthWiseSeparableConv(in_, out, padding=1),
+    ])
